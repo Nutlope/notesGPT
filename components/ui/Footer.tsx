3de620723fb75a00f46ceb1a29c54e34f882b3a7
nutlope <hassan4709@gmail.com>
2023-12-18 15:04:55 -0500

    made footer a bit bigger

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="container mx-auto my-5 flex h-16 flex-col items-center justify-between space-y-3 border-t px-3 pt-4 text-center sm:h-20 sm:flex-row sm:pt-2">
+    <footer className="container mx-auto my-5 flex h-16 flex-col items-center justify-between space-y-3 border-t px-3 pt-4 text-center sm:h-20 sm:flex-row sm:pt-2 md:text-lg">
       <div>
         Powered by{' '}
         <a
