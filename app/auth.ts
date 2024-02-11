fcbb45107b824682c8077bb4336a5505d1aaa625
Hassan El Mghari <hassan4709@gmail.com>
2024-02-11 14:35:37 -0500

    some changes

--- app/auth.ts
+++ app/auth.ts
@@ -1,8 +1,5 @@
 import { auth } from '@clerk/nextjs';
 
 export async function getAuthToken() {
-  const { userId, getToken } = auth();
-  const token = await getToken({ template: 'convex' });
-  console.log({ userId, token });
   return (await auth().getToken({ template: 'convex' })) ?? undefined;
 }
