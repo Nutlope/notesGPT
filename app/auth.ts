40b838037ac9166eb945ace7b03edb1b047a00a7
Hassan El Mghari <hassan4709@gmail.com>
2024-02-12 15:06:03 -0500

    added log to auth token

--- app/auth.ts
+++ app/auth.ts
@@ -1,5 +1,6 @@
 import { auth } from '@clerk/nextjs';
 
 export async function getAuthToken() {
+  console.log('auth token is', await auth().getToken({ template: 'convex' }));
   return (await auth().getToken({ template: 'convex' })) ?? undefined;
 }
