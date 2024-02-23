16093385db79f1a2ec2e438d92896eb79b49ec9c
Ian Macartney <ianhmacartney@gmail.com>
2024-02-22 17:41:14 -0800

    fix id type

--- app/recording/[id]/page.tsx
+++ app/recording/[id]/page.tsx
@@ -2,10 +2,9 @@ import { api } from '@/convex/_generated/api';
 import { preloadQuery } from 'convex/nextjs';
 import { getAuthToken } from '@/app/auth';
 import RecordingPage from './recording';
-import Header from '@/components/ui/Header';
+import { Id } from '@/convex/_generated/dataModel';
 
-const Page = async ({ params }: { params: { id: string } }) => {
-  const id = params.id as any;
+const Page = async ({ params: { id } }: { params: { id: Id<'notes'> } }) => {
   const token = await getAuthToken();
   const preloadedNote = await preloadQuery(
     api.notes.getNote,
